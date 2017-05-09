ec234b2b274c27120091ccc4b4a57e5deeb4785e
林奕儒 <gn301046@outlook.com>
2017-05-09 21:07:58 +0800

    if login success , guide to index.php

--- php/login.php
+++ php/login.php
@@ -1,13 +1,19 @@
 <?php
-
+	session_start();
 	$connection = mysqli_connect("db.mis.kuas.edu.tw","s1104137130","1314520","s1104137130");
 	$MEMber_account = $_POST['MEMBER_ACCOUNT'];
 	$MEMber_password = $_POST['MEMBER_PASSWORD'];
 
-	$sql = "SELECT MEMBER_ACCOUNT,MEMBER_PASSWORD FROM `MEMBER` WHERE MEMBER_ACCOUNT LIKE '".$MEMber_account."' AND MEMBER_PASSWORD LIKE '".$MEMber_password."'";
+	$sql = "SELECT MEMBER_ID,MEMBER_ACCOUNT,MEMBER_PASSWORD,MEMBER_NAME FROM `MEMBER` WHERE MEMBER_ACCOUNT LIKE '".$MEMber_account."' AND MEMBER_PASSWORD LIKE '".$MEMber_password."'";
+	mysqli_query ($connection,"SET NAMES UTF8"); 
 	$result = mysqli_query($connection,$sql);
+	$data = mysqli_fetch_array($result);
+
 	if(mysqli_num_rows($result)==1){
-		echo "OK";
+		$_SESSION["username"] = $data[3];
+		$_SESSION["userid"] = $data[0];
+		setcookie("username",$data[3],time()+3600);
+		header("Location: ../index.php");
 	}else{
 		echo "No";
 	}
