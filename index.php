f2871f31e6678a2d489898e42841b3442d28bc8e
林奕儒 <gn301046@outlook.com>
2017-05-09 21:18:27 +0800

    delete dist directory and modified register.css and sample.css and some index.php view

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 ﻿<?php
+	session_start();
 	include './php/searchDetail.php';
+	$login_form = "<form name='memberlogin' action='php/login.php' method='POST'>";
+	$login_form .= "<img src=\"PIC/top/account.png\" width=\"70px\" />";
+	$login_form .= "<input type=\"text\" name=\"MEMBER_ACCOUNT\" /></br>";
+	$login_form .= "<img src=\"PIC/top/password.png\" width=\"70px\" />";
+	$login_form .= "<input type=\"password\" name=\"MEMBER_PASSWORD\"></br>";
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +20,7 @@
 				<table>
 					<tr>
 						<td id="logo">
-							<a href="index.html"><img src="PIC/top/logo.png" width="200px"></a>
+							<a href="index.php"><img src="PIC/top/logo.png" width="200px"></a>
 						</td>
 						<td id="search">
 							<form name="search" action="php/search.php" method="GET">
@@ -33,31 +39,43 @@
 							</form>
 						</td>
 						<td id="memberlogin">
-							<form name="memberlogin" action="php/login.php" method="POST">
-								<img src="PIC/top/account.png" width="70px" />
-								<input type="text" name="MEMBER_ACCOUNT" />
-								<br>
-								<img src="PIC/top/password.png" width="70px" />
-								<input type="password" name="MEMBER_PASSWORD">
-								<br>
-							</form>
+							<?php
+								//判斷登入狀態
+								if(isset($_SESSION["username"])){
+									echo $_SESSION["username"]."</br>您好";
+								}else{
+									echo $login_form;	
+								}
+							?>
 						</td>
 						<td id="memberlogin2">
-							<a href="page/register.php"><img src="PIC/top/register.png" width="70px"></a><br>
-							<img src="PIC/top/login.png" onclick="document.memberlogin.submit()" width="70px"><br>
+							<?php
+								//判斷登入狀態
+								if(isset($_SESSION["username"])){
+									echo "<a href='./php/logout.php'/>登出";
+								}else{
+									echo "<a href=\"php/register.php\"><img src=\"PIC/top/register.png\" width=\"70px\"></a><br>";
+									echo "<img src=\"PIC/top/login.png\" onclick=\"document.memberlogin.submit()\" width=\"70px\"><br>";	
+								}
+							?>		
 						</td>
 					</tr>
 					<tr>
 						<td></td>
 						<td align="center">
-							<a href="page/movie.php" onMouseOut="document.movie.src='PIC/top/movie.png'" onMouseOver="document.movie.src='PIC/top/movie-1.png'"><img src="PIC/top/movie.png" name="movie" width="70px"></a>
-							<a href="page/drama.php" onMouseOut="document.drama.src='PIC/top/drama.png'" onMouseOver="document.drama.src='PIC/top/drama-1.png'"><img src="PIC/top/drama.png" name="drama" width="70px"></a>
-							<a href="page/tvshow.php" onMouseOut="document.tvshow.src='PIC/top/tvshow.png'" onMouseOver="document.tvshow.src='PIC/top/tvshow-1.png'"><img src="PIC/top/tvshow.png" name="tvshow" width="70px"></a>
-							<a href="page/actor.php" onMouseOut="document.actor.src='PIC/top/actor.png'" onMouseOver="document.actor.src='PIC/top/actor-1.png'"><img src="PIC/top/actor.png" name="actor" width="70px"></a>
+							<a href="php/movie.php" onMouseOut="document.movie.src='PIC/top/movie.png'" onMouseOver="document.movie.src='PIC/top/movie-1.png'"><img src="PIC/top/movie.png" name="movie" width="70px"></a>　
+							<a href="php/drama.php" onMouseOut="document.drama.src='PIC/top/drama.png'" onMouseOver="document.drama.src='PIC/top/drama-1.png'"><img src="PIC/top/drama.png" name="drama" width="70px"></a>　
+							<a href="php/tvshow.php" onMouseOut="document.tvshow.src='PIC/top/tvshow.png'" onMouseOver="document.tvshow.src='PIC/top/tvshow-1.png'"><img src="PIC/top/tvshow.png" name="tvshow" width="70px"></a>　
+							<a href="php/actor.php" onMouseOut="document.actor.src='PIC/top/actor.png'" onMouseOver="document.actor.src='PIC/top/actor-1.png'"><img src="PIC/top/actor.png" name="actor" width="70px"></a>
 						</td>
 						<td></td>
 						<td>
-							<a href="page/forget.html"><img src="PIC/top/forget.png" width="130px" /></a>
+						<?php
+							//判斷登入狀態
+							if(!isset($_SESSION["username"])){
+								echo "<a href=\"php/forget.php\"><img src=\"PIC/top/forget.png\" width=\"130px\" /></a>";
+							}
+						?>	
 						</td>
 					</tr>
 				</table>
