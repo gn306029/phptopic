f8d9793ee33428506e4390ac6d0f25b50bf06e27
林奕儒 <gn301046@outlook.com>
2017-05-13 17:31:25 +0800

    Update css

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	$login_form .= "<input type=\"text\" name=\"MEMBER_ACCOUNT\" /></br>";
 	$login_form .= "<img src=\"PIC/top/password.png\" width=\"70px\" />";
 	$login_form .= "<input type=\"password\" name=\"MEMBER_PASSWORD\"></br>";
+	$login_form .= "</form>";
 ?>
 
 <!DOCTYPE html>
@@ -39,37 +40,38 @@
 							</form>
 						</td>
 						<!--帳號密碼-->
-						<td id="memberlogin">
-							<?php
-								//判斷登入狀態
-								if(isset($_SESSION["username"])){
-									echo $_SESSION["username"]."</br>您好";
-									echo "<a href='./php/Member_Manager.php'>會員中心</a>";
-								}else{
-									echo $login_form;   
-								}
-							?>
-						</td>
-						<!--註冊-->
-						<td id="memberlogin2">
-							<?php
-								//判斷登入狀態
-								if(isset($_SESSION["username"])){
-									echo "<a href='./php/Member_Logout.php'/>登出";
-								}else{
-									echo "<a href=\"./php/Member_Register.php\"><img src=\"./PIC/top/register.png\" width=\"70px\"></a><br>";
-									echo "<img src=\"./PIC/top/login.png\" onclick=\"document.memberlogin.submit()\" width=\"70px\"><br>";    
-								}
-							?>
-						</td>
+                    <td id="memberlogin">
+                        <?php
+                            //判斷登入狀態
+                            if(isset($_SESSION["username"])){
+                                echo $_SESSION["username"].",您好<br>";
+								echo " <a href=./php/Member_Manager.php><img src=./PIC/top/manager-1.png name=manager width=150px></a>　　";
+								echo "<a href='./php/Member_Logout.php'/><img src=\"./PIC/top/logout.png\" width=\"70px\"></a>";
+                            }else{
+                                echo $login_form;   
+                            }
+                        ?>
+                    </td>
+                    <!--註冊-->
+                    <td id="memberlogin2">
+                        <?php
+                            //判斷登入狀態
+                            if(isset($_SESSION["username"])){
+                               
+                            }else{
+                                echo "<a href=\"./php/Member_Register.php\"><img src=\"./PIC/top/register.png\" width=\"70px\"></a><br>";
+                                echo "<img src=\"./PIC/top/login.png\" onclick=\"document.memberlogin.submit()\" width=\"70px\"><br>";    
+                            }
+                        ?>
+                    </td>
 					</tr>
 					<tr>
 						<td></td>
 						<td align="center">
-							<a href="php/Page_Movie.php" onMouseOut="document.movie.src='PIC/top/movie.png'" onMouseOver="document.movie.src='PIC/top/movie-1.png'"><img src="PIC/top/movie.png" name="movie" width="70px"></a>　
-							<a href="php/Page_Drama.php" onMouseOut="document.drama.src='PIC/top/drama.png'" onMouseOver="document.drama.src='PIC/top/drama-1.png'"><img src="PIC/top/drama.png" name="drama" width="70px"></a>　
-							<a href="php/Page_Tvshow.php" onMouseOut="document.tvshow.src='PIC/top/tvshow.png'" onMouseOver="document.tvshow.src='PIC/top/tvshow-1.png'"><img src="PIC/top/tvshow.png" name="tvshow" width="70px"></a>　
-							<a href="php/Page_Actor.php" onMouseOut="document.actor.src='PIC/top/actor.png'" onMouseOver="document.actor.src='PIC/top/actor-1.png'"><img src="PIC/top/actor.png" name="actor" width="70px"></a>
+							<a href="php/Page_Movie.php?search=&kind=1&category=0" onMouseOut="document.movie.src='PIC/top/movie.png'" onMouseOver="document.movie.src='PIC/top/movie-1.png'"><img src="PIC/top/movie.png" name="movie" width="70px"></a>　
+							<a href="php/Page_Drama.php?search=&kind=3&category=0" onMouseOut="document.drama.src='PIC/top/drama.png'" onMouseOver="document.drama.src='PIC/top/drama-1.png'"><img src="PIC/top/drama.png" name="drama" width="70px"></a>　
+							<a href="php/Page_Tvshow.php?search=&kind=2&category=0" onMouseOut="document.tvshow.src='PIC/top/tvshow.png'" onMouseOver="document.tvshow.src='PIC/top/tvshow-1.png'"><img src="PIC/top/tvshow.png" name="tvshow" width="70px"></a>　
+							<a href="php/Page_ActorList.php" onMouseOut="document.actor.src='PIC/top/actor.png'" onMouseOver="document.actor.src='PIC/top/actor-1.png'"><img src="PIC/top/actor.png" name="actor" width="70px"></a>
 						</td>
 						<td></td>
 						<td>
