6b011bf747d93c3f05aa5116099e071d11ca2f27
林奕儒 <gn301046@outlook.com>
2017-05-31 23:11:29 +0800

    will checkspecial into a separate file

---
+++ js/checkspecial.js
@@ -0,0 +1,14 @@
+function checkspecial(val) {
+    var toalarm = false;
+    var ch;
+    var stralarm = new Array("<", ">", ".", "!", "'", "/", "\\");
+    for (var i = 0; i < stralarm.length; i++) { //依序載入使用者輸入的每個字元
+        for (var j = 0; j < val.length; j++) {
+            ch = val.substr(j, 1);
+            if (ch == stralarm[i]) //如果包含禁止字元
+            {
+                return true;
+            }
+        }
+    }
+}
