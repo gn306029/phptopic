7717a835e028209103e54d1cf017fc4ff8c3c382
林奕儒 <gn301046@outlook.com>
2017-06-01 19:41:43 +0800

    repair chinese val bug

--- js/checkspecial.js
+++ js/checkspecial.js
@@ -1,14 +1,14 @@
-function checkspecial(val) {
-    var toalarm = false;
-    var ch;
-    var stralarm = new Array("<", ">", ".", "!", "'", "/", "\\");
-    for (var i = 0; i < stralarm.length; i++) { //依序載入使用者輸入的每個字元
-        for (var j = 0; j < val.length; j++) {
-            ch = val.substr(j, 1);
-            if (ch == stralarm[i]) //如果包含禁止字元
-            {
-                return true;
-            }
-        }
-    }
+function checkspecial(val){
+	var check=false;
+	for(var i = 0;i<val.length;i++){
+		if((val.charCodeAt(i)<=57 && val.charCodeAt(i)>=48)||
+		(val.charCodeAt(i)<=90 && val.charCodeAt(i)>=65)||
+		(val.charCodeAt(i)<=122 && val.charCodeAt(i)>=97)){
+		  
+		}else{
+			check=true;
+			break;
+		}
+	}
+	return check;
 }
